9e96919b64afadd475084f15c45d74a14badad17
Aníbal <anibal.alvarezg@gmail.com>
2024-07-09 11:40:10 -0400

    Update gpt controller

--- src/gpt/gpt.controller.ts
+++ src/gpt/gpt.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { GptService } from './gpt.service';
+import { OrthogaphyDto } from './dtos';
 
 @Controller('gpt')
 export class GptController {
   constructor(private readonly gptService: GptService) {}
+
+  @Post('orthography-check')
+  orthographyCheck(@Body() orthographyDto: OrthogaphyDto) {
+    return this.gptService.orthographyCheck(orthographyDto);
+  }
 }
--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { orthographyCheckUseCase } from './use-cases';
+import { OrthogaphyDto } from './dtos';
 
 @Injectable()
-export class GptService {}
+export class GptService {
+  async orthographyCheck(orthographyDto: OrthogaphyDto) {
+    return await orthographyCheckUseCase({
+      prompt: orthographyDto.prompt,
+    });
+  }
+}
